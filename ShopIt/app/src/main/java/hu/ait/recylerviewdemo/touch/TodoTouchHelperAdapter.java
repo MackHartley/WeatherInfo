35a4c008dd5bccc4f642b3cee306bed00762c5ff
MackHartley <mhartley@macalester.edu>
2017-04-15 03:18:59 +0200

    removed shopit

--- ShopIt/app/src/main/java/hu/ait/recylerviewdemo/touch/TodoTouchHelperAdapter.java
+++
@@ -1,8 +0,0 @@
-package hu.ait.recylerviewdemo.touch;
-
-public interface TodoTouchHelperAdapter {
-
-    void onItemDismiss(int position);
-
-    void onItemMove(int fromPosition, int toPosition);
-}
